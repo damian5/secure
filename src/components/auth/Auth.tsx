ae0f9772281b7205fdd17a1137b3645601ae9bea
damian5 <damian.lingua1@gmail.com>
2021-01-10 17:45:22 +0100

    pin system: auth screen (wip)

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { webAuthnSignin, webAuthnSignup } from 'helpers/webauth';
 import { FingerPrintContext } from 'hooks/useFingerPrint';
 import { useFirebaseAuth } from 'hooks/useFirebaseAuth';
@@ -8,17 +8,47 @@ import { AuthContext } from 'hooks/authContext';
 const Auth = () => {
   const { useFingerPrint, error } = useContext(FingerPrintContext);
   const { isFirebaseReady, authenticated } = useContext(AuthContext);
+  const [ securityNumber, setSecurityNumber ] = useState('')
+  const [ didFingerprintFail, setDidFingerprintFail ] = useState(false)
+  const [ passwordError, setPasswordError ] = useState('')
+  const [ showPad, setShowPad] = useState(false)
+
+  
   const { signOut } = useFirebaseAuth();
   const history = useHistory();
+
+
+  console.log('render');
+  const fakeSecurityNumber = 2323
+
+  useEffect(() => {
+
+    if(securityNumber.length === 4) {
+      if(fakeSecurityNumber === Number(securityNumber)) {
+        history.push('/passwords')
+      } else {
+        setPasswordError('wrong password')
+      }
+    } else if (securityNumber.length > 4) {
+      setPasswordError('')
+      setSecurityNumber('')
+    } else {
+      setPasswordError('')
+    }
+  }, [securityNumber])
 
   useEffect(() => {
     if(authenticated) {
       if(useFingerPrint === 'enable') {
+        if(didFingerprintFail) {
+          return;
+        }
         const usersString = localStorage.getItem('users');
         if(usersString) {
         webAuthnSignin()
           .then(() => history.push('/passwords'))
           .catch(error => {
+            setDidFingerprintFail(true)
             console.log(error);
           })
         } else {
@@ -32,9 +62,15 @@ const Auth = () => {
     }
 }, [history, authenticated, useFingerPrint, isFirebaseReady, signOut]);
 
+  const triggerChange = (e: any) => {
+    setSecurityNumber(e.target.value)
+  }
+  
   return (
     <div>
-      {error ? 'error' : 'Fingerprint required'}
+      <p>Fingerprint required or <p onClick={() => setShowPad(true)}>insert number</p></p>
+      {showPad && <input type="number" name="test" value={securityNumber} onChange={(e) => triggerChange(e)}/>}
+      <p>{passwordError}</p>
     </div>
   )
 }
